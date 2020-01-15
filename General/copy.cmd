039fb89f43cb45142e85396445fe9d84fc9a3a48
alexvarboffin@gmail.com <~jO78L;09zx^&982>
2020-01-15 20:18:30 +0200

    123

--- General/copy.cmd
+++ General/copy.cmd
@@ -1,2 +1,4 @@
 copy subdomains.txt "C:\ProgramData\Acunetix Manual Tools\Data\General\subdomains.txt"
+copy subdomains.txt "C:\ProgramData\Acunetix WVS 10\Data\General\subdomains.txt"
+copy subdomains.txt "D:\bin\OSINT\Foca-v3.4.3\DNSDictionary\hosts.txt"
 pause
